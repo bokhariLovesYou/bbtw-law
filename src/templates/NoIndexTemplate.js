ceb618644312affa6eecaff042dbdc3ceea8491b
Admin <admin@MacBook-Pro-2.local>
2020-03-01 04:47:42 -0600

    adding the firm

--- src/templates/NoIndexTemplate.js
+++ src/templates/NoIndexTemplate.js
@@ -3,7 +3,6 @@ import SEO from "../components/seo"
 
 export class NoIndexTemplate extends Component {
   render() {
-    console.log(this.props)
     return (
       <div>
         <SEO title="Noindex, Nofollow" noIndex="noindex, nofollow" />
